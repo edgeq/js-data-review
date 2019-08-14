c93724d7bf35dee549c0f4f7066b9d668762769c
edgeq <edgarq86@gmail.com>
2019-08-14 17:50:18 -0500

    finished up need to do this in react

--- 02-iss-geolocation/iss.js
+++ 02-iss-geolocation/iss.js
@@ -1,11 +1,10 @@
+// Make map and tiles
 const issMap = L.map("issMap").setView([0, 0], 1);
-const marker = L.marker([0, 0]).addTo(issMap);
-
 const attr =
   'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>';
-
 const tileUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
 const tiles = L.tileLayer(tileUrl, { attr });
+const marker = L.marker([0, 0]).addTo(issMap);
 
 tiles.addTo(issMap);
 
@@ -14,10 +13,11 @@ async function getISS() {
   const res = await fetch(api_url);
   const { latitude, longitude } = await res.json();
   marker.setLatLng([latitude, longitude]);
-  // L.marker([latitude, longitude]).addTo(issMap);
+  issMap.setView([latitude, longitude], 2);
 
-  document.getElementById("lat").textContent = latitude;
-  document.getElementById("lon").textContent = longitude;
+  document.getElementById("lat").textContent = latitude.toFixed(2);
+  document.getElementById("lon").textContent = longitude.toFixed(2);
 }
 
 getISS();
+setInterval(getISS, 2000);
