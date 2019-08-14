e1f640881d86b6e1222aeefdf5f6e3ae4b19a902
edgeq <edgarq86@gmail.com>
2019-08-14 17:21:32 -0500

    pulling from api and printing to map

--- 02-iss-geolocation/iss.js
+++ 02-iss-geolocation/iss.js
@@ -1,7 +1,20 @@
+const issMap = L.map("issMap").setView([0, 0], 1);
+const marker = L.marker([0, 0]).addTo(issMap);
+
+const attr =
+  'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>';
+
+const tileUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+const tiles = L.tileLayer(tileUrl, { attr });
+
+tiles.addTo(issMap);
+
 async function getISS() {
   const api_url = "https://api.wheretheiss.at/v1/satellites/25544";
   const res = await fetch(api_url);
   const { latitude, longitude } = await res.json();
+  marker.setLatLng([latitude, longitude]);
+  // L.marker([latitude, longitude]).addTo(issMap);
 
   document.getElementById("lat").textContent = latitude;
   document.getElementById("lon").textContent = longitude;
