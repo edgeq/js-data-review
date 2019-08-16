ad3e7fe696430b85cb7aa284f7cbe050b98af7ac
Edge Quintanilla <edgarq86@gmail.com>
2019-08-15 23:41:12 -0500

    set up api calls in charts

---
+++ chart-js-react/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import BasicChart from "./BasicChart";
+import TempChart from "./TempChart";
+import axios from "axios";
+import "./App.css";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.getData = this.getData.bind(this);
+  }
+  async getData() {
+    await fetch("https://jsonplaceholder.typicode.com/posts?limit=10")
+      .then(res => res.json())
+      .then(data => console.log(data));
+  }
+  componentDidMount() {
+    this.getData();
+  }
+  render() {
+    return (
+      <div className="App">
+        <BasicChart />
+        <TempChart data={this.getData} />
+      </div>
+    );
+  }
+}
---
+++ chart-js-react/src/TempChart.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { Line } from "react-chartjs-2";
+
+export class TempChart extends Component {
+  render() {
+    return (
+      <div className="TempChart">
+        <h1>TempChart Goes Here</h1>
+      </div>
+    );
+  }
+}
+
+export default TempChart;
